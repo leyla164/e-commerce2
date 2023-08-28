390310c64a99866e9b3aef0bed5d17ffa5cfded8
leyla164 <leylamaisarahdasni@gmail.com>
2023-08-29 06:28:11 +0700

    order2

---
+++ src/components/Navbar.vue
@@ -0,0 +1,87 @@
+<template>
+    <div>
+      <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a href="https://flowbite.com/" class="flex items-center">
+            <img src="../assets/img/gambar.jpg" class="h-8 mr-3" alt="Flowbite Logo">
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">AbabaS</span>
+          </a>
+          <div class="flex md:order-2">
+
+            <div v-if="isAuthenticated">
+              <router-link to="/cart-checkout" class="relative inline-flex items-center mr-4 px-5">
+              <router-link to="/cart" class="relative inline-flex items-center mr-4 px-5">
+    
+              
+              </router-link>
+             </router-link>
+  
+            </div>
+            <div v-if="isAuthenticated" class="flex md:order-2">
+                 <CartBadgeComponent/>
+              <div >
+                <a @click="logout"
+                  class="text-white  cursor-pointer bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 -top-9 md:mr-0  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Logout</a>
+              </div>
+            </div>
+            <div v-else>
+              <a href="/login"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</a>
+            </div>
+            <button data-collapse-toggle="navbar-sticky" type="button"
+              class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-sticky" aria-expanded="false">
+              <span class="sr-only">Open main menu</span>
+              <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  d="M1 1h15M1 7h15M1 13h15" />
+              </svg>
+            </button>
+          </div>
+          <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+            <ul
+              class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <a href="/"
+                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Branda</a>
+              </li>
+              <li>
+                <a href="/produk"
+                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Produk</a>
+              </li>
+              <li>
+                <a href="/categories"
+                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Kategori</a>
+              </li>
+              <li>
+                <a href="/brands"
+                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Merk</a>
+              </li>
+              <li>
+                <a href="/contact"
+                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Kontak</a>
+              </li>
+              <li>
+                <a href="/profil"
+                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Profil</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </template>
+    
+    <script>
+    import { mapActions, mapGetters } from 'vuex';
+    import CartBadgeComponent from './CartBadgeComponent.vue';
+    export default {
+      computed: {
+            ...mapGetters('auth', ['isAuthenticated']),
+        },
+        methods: {
+            ...mapActions('auth', ['logout']),
+        },
+        components: { CartBadgeComponent }
+    };
+    </script>
---
+++ src/views/Home.vue
@@ -0,0 +1,117 @@
+<template>
+<br><br><br>
+<div class="mx-auto my-10 w-screen max-w-screen-xl px-4">
+  <div class="flex rounded-xl bg-white py-2 px-2 text-gray-700 shadow-lg shadow-pink-200">
+    <div class="my-auto mr-4 p-3 text-blue-500">
+      <h1 class="text-2xl font-bold">Skincare</h1>
+      <p class="mt-4 text-lg"> produk perawatan kulit yang memiliki tujuan mulai dari menenangkan, memulihkan, memperbaiki, hingga melindungi kulit.</p>
+      <button type="submit" class="group mt-4 mr-2 mb-4 flex cursor-pointer text-left text-xl font-semibold leading-tight text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" class="group-hover:ml-8 ml-4 h-7 w-7 transition-all" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
+        </svg>
+      </button>
+    </div>
+    <div class="hidden h-72 w-1/2 flex-shrink-0 justify-center lg:flex">
+      <img class="" src="../assets/img/pyunkang.png" alt="" />
+    </div>
+  </div>
+</div>
+
+<br><br>
+ <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="../assets/img/AHA.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="../assets/img/cd.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="../assets//img/crox.jpg" alt="">
+    </div>
+</div>
+
+<section class="py-20">
+  <h1 class="mb-12 text-center font-sans text-5xl font-bold"></h1>
+  <div class="mx-auto grid max-w-screen-lg grid-cols-1 gap-5 p-5 sm:grid-cols-2 md:grid-cols-3 lg:gap-10">
+    
+    <article class="h-90 col-span-1 m-auto min-h-full cursor-pointer overflow-hidden rounded-lg pb-2 shadow-lg transition-transform duration-200 hover:translate-y-2">
+      <a href="#" class="block h-full w-full">
+        <img class="max-h-40 w-full object-cover" alt="featured image" src="../assets/img/12.png" />
+        <div class="w-full bg-white p-4">
+          <p class="text-md font-medium text-indigo-500">Serum</p>
+          <p class="mb-2 text-xl font-medium text-gray-800">Clarifying Serum</p>
+          <p class="text-md font-light text-gray-400">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse vel neque ipsam?</p>
+          <div class="justify-starts mt-4 flex flex-wrap items-center">
+            <div class="mr-2 mt-1 rounded-2xl bg-blue-100 py-1.5 px-4 text-xs text-gray-600">#js</div>
+            <div class="mr-2 mt-1 rounded-2xl bg-blue-100 py-1.5 px-4 text-xs text-gray-600">#icefactory</div>
+          </div>
+        </div>
+      </a>
+    </article>
+    
+    <article class="h-90 col-span-1 m-auto min-h-full cursor-pointer overflow-hidden rounded-lg pb-2 shadow-lg transition-transform duration-200 hover:translate-y-2">
+      <a href="#" class="block h-full w-full">
+        <img class="max-h-40 w-full object-cover" alt="featured image" src="../assets/img/34.jpg" />
+        <div class="w-full bg-white p-4">
+          <p class="text-md font-medium text-indigo-500">Gardening</p>
+          <p class="mb-2 text-xl font-medium text-gray-800">Sunflowers are my favorite</p>
+          <p class="text-md font-light text-gray-400">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse vel neque ipsam?</p>
+          <div class="justify-starts mt-4 flex flex-wrap items-center">
+            <div class="mr-2 mt-1 rounded-2xl bg-blue-100 py-1.5 px-4 text-xs text-gray-600">#js</div>
+            <div class="mr-2 mt-1 rounded-2xl bg-blue-100 py-1.5 px-4 text-xs text-gray-600">#icefactory</div>
+          </div>
+        </div>
+      </a>
+    </article>
+    
+    <article class="h-90 col-span-1 m-auto min-h-full cursor-pointer overflow-hidden rounded-lg pb-2 shadow-lg transition-transform duration-200 hover:translate-y-2">
+      <a href="#" class="block h-full w-full">
+        <img class="max-h-40 w-full object-cover" alt="featured image" src="../assets/img/33.jpg" />
+        <div class="w-full bg-white p-4">
+          <p class="text-md font-medium text-indigo-500">Coding</p>
+          <p class="mb-2 text-xl font-medium text-gray-800">Getting to know the Ice Factory Pattern</p>
+          <p class="text-md font-light text-gray-400">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse vel neque ipsam?</p>
+          <div class="justify-starts mt-4 flex flex-wrap items-center">
+            <div class="mr-2 mt-1 rounded-2xl bg-blue-100 py-1.5 px-4 text-xs text-gray-600">#js</div>
+            <div class="mr-2 mt-1 rounded-2xl bg-blue-100 py-1.5 px-4 text-xs text-gray-600">#icefactory</div>
+          </div>
+        </div>
+      </a>
+    </article>
+  </div>
+</section>
+
+
+<br><br>
+ <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="../assets/img/77.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="../assets/img/55.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="../assets//img/66.jpg" alt="">
+    </div>
+</div>
+
+<br>
+
+<div class="grid grid-cols-1 md:grid-cols-3 gap-4 m-20">
+<video class="w-96" controls autoplay muted>
+  <source src="../assets/vidio/vidio.mp4"  type="video/mp4">
+  Your browser does not support the video tag.
+</video>
+<video class="w-96" controls autoplay muted>
+  <source src="../assets/vidio/vidio2.mp4" type="video/mp4">
+  Your browser does not support the video tag.
+</video>
+<video class="w-96" controls autoplay muted>
+  <source src="../assets/vidio/vidio3.mp4" type="video/mp4">
+  Your browser does not support the video tag.
+</video>
+</div> 
+
+
+</template>
+
